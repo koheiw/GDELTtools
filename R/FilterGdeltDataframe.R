cf51bfab3587e3535e1261023a28283f2f36e0cb
Stephen R. Haptonstahl <srh@haptonstahl.org>
2021-11-26 13:00:06 +0000

    version 1.5

--- R/FilterGdeltDataframe.R
+++ R/FilterGdeltDataframe.R
@@ -1,49 +1,20 @@
 FilterGdeltDataframe <- function(x, 
-                                 filter, 
-                                 allow.wildcards=FALSE, 
-                                 use.regex=FALSE,
-                                 verbose=TRUE) {
+                                 verbose,
+                                 row_filter,
+                                 ...) {
   # 'or' within values for a field, 'and' across fields
   #
-  # ex: FilterGdeltDataframe(my.df, list(ActionGeo_ADM1Code=c("NI", "US"), ActionGeo_CountryCode="US"))
+  # ex: FilterGdeltDataframe(my_df, list(ActionGeo_ADM1Code=c("NI", "US"), ActionGeo_CountryCode="US"))
   # This keeps rows with (ActionGeo_ADM1Code=NI AND ActionGeo_CountryCode=US) OR
   #   (ActionGeo_ADM1Code=US AND ActionGeo_CountryCode=US)
-  if(verbose) cat("Filtering\n\n")
-  if(use.regex) {
-    filter.results <- laply(1:length(filter), function(fi) {
-      field.results <- laply(.data=filter[[fi]], .fun=function(v) {
-        grepl(v, x[names(filter)[fi]][,1])
-      }, .drop=FALSE)
-      if(is.array(field.results)) return(apply(field.results, 2, any))
-      else return(field.results)
-    })
-  } else if(allow.wildcards) {
-    filter.results <- laply(1:length(filter), function(fi) {
-      field.results <- laply(.data=filter[[fi]], .fun=function(v) {
-        v <- gsub("*", "[:alnum:]*", v, fixed=TRUE)
-        grepl(v, x[names(filter)[fi]][,1])
-      }, .drop=FALSE)
-      if(is.array(field.results)) return(apply(field.results, 2, any))
-      else return(field.results)
-    })
+  
+  if(verbose) cat("Filtering\n")
+  x <- dplyr::filter(x, {{row_filter}})
+  if(0==...length()) {
+    cat("Selecting all columns\n\n")
+    return(x)
   } else {
-    filter.results <- laply(1:length(filter), function(fi) {
-      field.results <- laply(.data=filter[[fi]], .fun=function(v) x[names(filter)[fi]]==v, .drop=FALSE)
-      if(is.array(field.results)) return(apply(field.results, 2, any))
-      else return(field.results)
-    })
+    cat("Selecting some columns using",...length(),"argument(s)\n")
+    return( dplyr::select(x, ...) )
   }
-  
-  if(is.array(filter.results)) rows.to.keep <- apply(filter.results, 2, all)
-  else rows.to.keep <- filter.results
-  
-  out <- x[rows.to.keep,]
-  
-  # remove NA values for filtered fields
-  for(i in 1:length(filter)) {
-    keep.rows <- !is.na(out[,names(filter)[i]])
-    out <- out[keep.rows,]
-  }
-  
-  return(out)
 }
--- R/GetGdelt.R
+++ R/GetGdelt.R
@@ -1,39 +1,43 @@
 #' Download and subset GDELT data
 #'
-#' Download the GDELT files necessary for a data set, import them,
-#' filter on various crieteria, and return a data.frame. 
+#' Download the GDELT files necessary for a data set, import them, filter on various criteria, and return a data.frame. 
 #' 
 #' @aliases GetGDELT
-#' @param start.date character, just about any human-readable form of the earliest date to include.
-#' @param end.date character, just about any human-readable form of the latest date to include.
-#' @param filter list, named list encoding the values to include for specified fields. See Details.
-#' @param local.folder character, if specified, where downloaded files will be saved.
-#' @param max.local.mb numeric, the maximum size in MB of the downloaded files that will be retained.
-#' @param allow.wildcards logical, must be TRUE to use * in \code{filter} to specify 'any character(s)'.
-#' @param use.regex logical, if TRUE then \code{filter} will be processed as a \code{\link{regular expression}}.
-#' @param data.url.root character, URL for the folder with GDELT data files.
-#' @param verbose logical, if TRUE then indications of progress will be displayed.
+#' @param start_date character, just about any human-readable form of the earliest date to include.
+#' @param end_date character, just about any human-readable form of the latest date to include.
+#' @param local_folder character, if specified, where downloaded files will be saved.
+#' @param max_local_mb numeric, the maximum size in MB of the downloaded files that will be retained.
+#' @param data_url_root character, URL for the folder with GDELT data files.
+#' @param verbose logical, if TRUE then indications of progress will be displayed_
+#' @param row_filter <data-masking> Row selection. Expressions that return a logical value, and are defined in terms of the variables in GDELT. If multiple expressions are included, they are combined with the & operator. Only rows for which all conditions evaluate to TRUE are kept.
+#' @param ... <tidy-select>, Column selection. This takes the form of one or more unquoted expressions separated by commas. Variable names can be used as if they were positions in the data frame, so expressions like x:y can be used to select a range of variables.
 #' @return data.frame
 #' @export
 #' @details
 #' 
-#' If \code{local.folder} is not specified then downloaded files are stored in
-#' \code{tempdir()}. If a needed file has already been downloaded to \code{local.folder}
-#' then this file is used instead of being downloaded. This can greatly speed up future
-#' 
-#' 
 #' Dates are parsed with \code{dateParse} in the TimeWarp package. 
 #' Years must be given with four digits.
+#'
+#' If \code{local_folder} is not specified then downloaded files are stored in
+#' \code{tempdir()}. If a needed file has already been downloaded to \code{local_folder}
+#' then this file is used instead of being downloaded. This can greatly speed up future
+#' downloads.
 #' 
 #' @section Filtering Results:
 #' 
-#' This is how you write the \code{filter}.
+#' The \code{row_filter} is passed to \code{\link[dplyr]{filter}}. This is a very flexible way to filter
+#' the rows. It's well worth checking out the \code{\link[dplyr]{filter}} documentation.
+#' 
+#' @section Selecting Columns:
+#' 
+#' The \code{...} is passed to \code{\link[dplyr]{select}}. This is a very flexible way to choose
+#' which columns to return. It's well worth checking out the \code{\link[dplyr]{select}} documentation.
 #' 
 #' @references
-#' GDELT: Global Data on Events, Location and Tone, 1979-2012.  
+#' GDELT: Global Data on Events, Location and Tone, 1979-2013.  
 #' Presented at the 2013 meeting of the International Studies Association
 #' in San Francisco, CA.
-#' \url{http://www.gdeltproject.org/}
+#' \url{https://www.gdeltproject.org/}
 #' @author 
 #' \tabular{ll}{
 #'   Stephen R. Haptonstahl \tab \email{srh@@haptonstahl.org}\cr
@@ -42,110 +46,114 @@
 #' }
 #' @examples
 #' \dontrun{
-#' test.filter <- list(ActionGeo_ADM1Code=c("NI", "US"), ActionGeo_CountryCode="US")
-#' test.results <- GetGDELT(start.date="1979-01-01", end.date="1979-12-31", 
-#'   filter=test.filter)
-#' table(test.results$ActionGeo_ADM1Code)
-#' table(test.results$ActionGeo_CountryCode)}
+#' df1 <- GetGDELT(start_date="1979-01-01", end_date="1979-12-31")
 #' 
+#' df2 <- GetGDELT(start_date="1979-01-01", end_date="1979-12-31",
+#'                 row_filter=ActionGeo_CountryCode=="US")
+#' 
+#' df3 <- GetGDELT(start_date="1979-01-01", end_date="1979-12-31",
+#'                 row_filter=Actor2Geo_CountryCode=="RS" & NumArticles==2 & is.na(Actor1CountryCode), 
+#'                 1:5)
+#' 
+#' df4 <- GetGDELT(start_date="1979-01-01", end_date="1979-12-31",
+#'                 row_filter=Actor2Code=="COP" | Actor2Code=="MED", 
+#'                 contains("date"), starts_with("actor"))
+#'
 #' # Specify a local folder to store the downloaded files
-#' \dontrun{
-#' test.results <- GetGDELT(start.date="1979-01-01", end.date="1979-12-31", 
-#'                          filter=test.filter,
-#'                          local.folder="~/gdeltdata",
-#'                          max.local.mb=500)}
-GetGDELT <- function(start.date,
-                     end.date=start.date,
-                     filter,
-                     local.folder=tempdir(),
-                     max.local.mb=Inf,
-                     allow.wildcards=FALSE, 
-                     use.regex=FALSE,
-                     data.url.root="http://data.gdeltproject.org/events/",
+#' df5 <- GetGDELT(start_date="1979-01-01", end_date="1979-12-31",
+#'                 row_filter=ActionGeo_CountryCode=="US",
+#'                 local_folder = "~/gdeltdata")
+#' }
+GetGDELT <- function(start_date,
+                     end_date=start_date,
+                     row_filter,
+                     ...,
+                     local_folder=tempdir(),
+                     max_local_mb=Inf,
+                     data_url_root="http://data.gdeltproject.org/events/",
                      verbose=TRUE) {
   
   # Coerce ending slashes as needed
-  local.folder <- StripTrailingSlashes(path.expand(local.folder))
-  data.url.root <- paste(StripTrailingSlashes(data.url.root), "/", sep="")
-  # create the local.folder if is doesn't exist
-  dir.create(local.folder, showWarnings=FALSE, recursive = TRUE)
   
-  start.date <- strftime(dateParse(start.date), format="%Y-%m-%d")
-  end.date <- strftime(dateParse(end.date), format="%Y-%m-%d")
+  local_folder <- StripTrailingSlashes(path.expand(local_folder))
+  data_url_root <- paste(StripTrailingSlashes(data_url_root), "/", sep="")
+  # create the local_folder if is doesn't exist
+  dir.create(local_folder, showWarnings=FALSE, recursive = TRUE)
   
-  if(start.date <= "2014-01-25" & end.date >= "2014-01-23") warning("Your date range includes some or all of Feb 23-25, 2014. GDELT data is not available for these dates.")
+  start_date <- strftime(dateParse(start_date), format="%Y-%m-%d")
+  end_date <- strftime(dateParse(end_date), format="%Y-%m-%d")
   
-  out.initialized <- FALSE
+  if(start_date <= "2014-01-25" & end_date >= "2014-01-23") warning("Your date range includes some or all of Feb 23-25, 2014. GDELT data is not available for these dates.")
+  
+  out_initialized <- FALSE
   
   # Determine file list based on dates
-  source.files <- FileListFromDates(start.date=start.date, end.date=end.date)
-  if(0 == length(source.files)) stop("No GDELT files available for the dates specified")
+  source_files <- FileListFromDates(start_date=start_date, end_date=end_date)
+  if(0 == length(source_files)) stop("No GDELT files available for the dates specified")
   
   # Ingest and filter local files
-  for(this.file in LocalVersusRemote(filelist=source.files, local.folder=local.folder)$local) {
-    if(FALSE == IsValidGDELT(f=this.file, local.folder=local.folder)) {
+  for(this_file in LocalVersusRemote(filelist=source_files, local_folder=local_folder)$local) {
+    if(FALSE == IsValidGDELT(f=this_file, local_folder=local_folder)) {
       # remove the offending file; it'll be downloaded in the later loop
-      file.remove(paste(local.folder, "/", this.file, sep=""))
+      file.remove(paste(local_folder, "/", this_file, sep=""))
       next
     }
-    new.data <- GdeltZipToDataframe(f=paste(local.folder, "/", this.file, sep=""),
-                                    daily=grepl("export.CSV", this.file, fixed=TRUE),
+    new_data <- GdeltZipToDataframe(f=paste(local_folder, "/", this_file, sep=""),
+                                    daily=grepl("export.CSV", this_file, fixed=TRUE),
                                     verbose=verbose)
-    if(!missing(filter)) new.data <- FilterGdeltDataframe(x=new.data,
-                                                          filter=filter,
-                                                          allow.wildcards=allow.wildcards,
-                                                          use.regex=use.regex,
-                                                          verbose=verbose)
-    if(out.initialized) {
-      out <- rbind(out, new.data)
+    if(!missing(row_filter) | ...length() > 0) new_data <- FilterGdeltDataframe(x=new_data,
+                                                                                verbose=verbose,
+                                                                                row_filter={{row_filter}},
+                                                                                ...=...)
+    if(out_initialized) {
+      out <- rbind(out, new_data)
     } else {
-      out <- new.data
-      out.initialized <- TRUE
+      out <- new_data
+      out_initialized <- TRUE
     }
   }
   
   # Download, ingest, and filter remote files
-  for(this.file in LocalVersusRemote(filelist=source.files, local.folder=local.folder)$remote) {
-    download.result <- DownloadGdelt(f=this.file,
-                                     local.folder=local.folder,
-                                     max.local.mb=max.local.mb,
-                                     data.url.root=data.url.root,
+  for(this_file in LocalVersusRemote(filelist=source_files, local_folder=local_folder)$remote) {
+    download_result <- DownloadGdelt(f=this_file,
+                                     local_folder=local_folder,
+                                     max_local_mb=max_local_mb,
+                                     data_url_root=data_url_root,
                                      verbose=verbose)
-    if(FALSE == download.result) {
-      stop("Unable to download file ", this.file, ". Please try again. If you get this result again, the file might not be available on the server.")
+    if(FALSE == download_result) {
+      stop("Unable to download file ", this_file, ". Please try again. If you get this result again, the file might not be available on the server.")
     }
-    if(FALSE == IsValidGDELT(f=this.file, local.folder=local.folder)) {
+    if(FALSE == IsValidGDELT(f=this_file, local_folder=local_folder)) {
       # try again
-      download.result <- DownloadGdelt(f=this.file,
-                                       local.folder=local.folder,
-                                       max.local.mb=max.local.mb,
-                                       data.url.root=data.url.root,
+      download_result <- DownloadGdelt(f=this_file,
+                                       local_folder=local_folder,
+                                       max_local_mb=max_local_mb,
+                                       data_url_root=data_url_root,
                                        verbose=verbose)
-      if(FALSE == IsValidGDELT(f=this.file, local.folder=local.folder)) {
-        stop("Unable to verify the integrity of ", this.file)
+      if(FALSE == IsValidGDELT(f=this_file, local_folder=local_folder)) {
+        stop("Unable to verify the integrity of ", this_file)
       }
     }
     
-    new.data <- GdeltZipToDataframe(f=paste(local.folder, "/", this.file, sep=""),
-                                    daily=grepl("export.CSV", this.file, fixed=TRUE),
+    new_data <- GdeltZipToDataframe(f=paste(local_folder, "/", this_file, sep=""),
+                                    daily=grepl("export.CSV", this_file, fixed=TRUE),
                                     verbose=verbose)
-    if(!missing(filter)) new.data <- FilterGdeltDataframe(x=new.data,
-                                                          filter=filter,
-                                                          allow.wildcards=allow.wildcards,
-                                                          use.regex=use.regex,
-                                                          verbose=verbose)
-    if(out.initialized) {
-      out <- rbind(out, new.data)
+    if(!missing(row_filter) | ...length() > 0) new_data <- FilterGdeltDataframe(x=new_data,
+                                                                                verbose=verbose,
+                                                                                row_filter={{row_filter}},
+                                                                                ...=...)
+    if(out_initialized) {
+      out <- rbind(out, new_data)
     } else {
-      out <- new.data
-      out.initialized <- TRUE
+      out <- new_data
+      out_initialized <- TRUE
     }
   }
   
   # Filter one more time on dates
-  start.date.numeric <- as.numeric(strftime(dateParse(start.date), format="%Y%m%d"))
-  end.date.numeric <- as.numeric(strftime(dateParse(end.date), format="%Y%m%d"))
-  out <- out[out$SQLDATE >= start.date.numeric & out$SQLDATE <= end.date.numeric,]
+  start_date_numeric <- as.numeric(strftime(dateParse(start_date), format="%Y%m%d"))
+  end_date_numeric <- as.numeric(strftime(dateParse(end_date), format="%Y%m%d"))
+  out <- out[out$SQLDATE >= start_date_numeric & out$SQLDATE <= end_date_numeric,]
   
   return(out)
 }
